64f06d82996e56b1eb18e71b5f0d7b143fb7cb62
rajhans92 <rupesh.rajhans92@gmail.com>
2021-07-01 12:33:04 +0530

    rebase qa branch to sumit branch

---
+++ controllers/userController.js
@@ -0,0 +1,125 @@
+// Create and Save a new User
+app.create = (req, res) => {
+    // Validate request
+    if (!req.body) {
+      res.status(400).send({
+        message: "Content can not be empty!"
+      });
+    }
+  
+    // Create a User
+    const user = new User({
+        "id": users.id,
+        "first_name": users.first_name,
+        "last_name": users.last_name,
+        "email": users.email,
+        "phone_no": users.phone_no,
+        "alt_phone_no": users.alt_phone_no,
+    });
+  
+    // Save User in the database
+    User.create(user, (err, data) => {
+      if (err)
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while creating the User."
+        });
+      else res.send(data);
+    });
+};
+
+
+
+// Retrieve all Users from the database.
+app.findAll = (req, res) => {
+    User.getAll((err, data) => {
+      if (err)
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving users."
+        });
+      else res.send(data);
+    });
+};
+
+
+
+// Find a single User with a userId
+app.findOne = (req, res) => {
+    User.findById(req.params.userId, (err, data) => {
+      if (err) {
+        if (err.kind === "not_found") {
+          res.status(404).send({
+            message: `Not found User with id ${req.params.userId}.`
+          });
+        } else {
+          res.status(500).send({
+            message: "Error retrieving User with id " + req.params.userId
+          });
+        }
+      } else res.send(data);
+    });
+};
+
+
+
+// Update a User identified by the userId in the request
+app.update = (req, res) => {
+    // Validate Request
+    if (!req.body) {
+      res.status(400).send({
+        message: "Content can not be empty!"
+      });
+    }
+  
+    User.updateById(
+      req.params.userId,
+      new User(req.body),
+      (err, data) => {
+        if (err) {
+          if (err.kind === "not_found") {
+            res.status(404).send({
+              message: `Not found User with id ${req.params.userId}.`
+            });
+          } else {
+            res.status(500).send({
+              message: "Error updating User with id " + req.params.userId
+            });
+          }
+        } else res.send(data);
+      }
+    );
+};
+
+
+
+// Delete a User with the specified userId in the request
+app.delete = (req, res) => {
+    User.remove(req.params.userId, (err, data) => {
+      if (err) {
+        if (err.kind === "not_found") {
+          res.status(404).send({
+            message: `Not found User with id ${req.params.userId}.`
+          });
+        } else {
+          res.status(500).send({
+            message: "Could not delete User with id " + req.params.userId
+          });
+        }
+      } else res.send({ message: `User was deleted successfully!` });
+    });
+};
+
+
+
+// Delete all Users from the database.
+app.deleteAll = (req, res) => {
+    User.removeAll((err, data) => {
+      if (err)
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while removing all users."
+        });
+      else res.send({ message: `All Users were deleted successfully!` });
+    });
+};
